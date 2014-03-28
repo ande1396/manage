ca4cc0d7495c53eefc2e281faf6135bb56f680fc
Kyles <ande1396@ubuntu.(none)>
2014-03-28 10:24:32 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+AdminUser.create(:first_name => "Kyle", :last_name => "Andeson", :email => "msuande1396@yahoo.com", :username => "ande1396", :password => "testpassword", :password_confirmation => "testpassword")
